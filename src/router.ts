f0c85acacb267e416f946527540551bf9d5b5598
Erdogan Bulut <ebulut@protel.com.tr>
2018-03-16 15:14:30 +0300

    Update router for created pages

--- src/router.ts
+++ src/router.ts
@@ -2,6 +2,13 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
 import About from './views/About.vue';
+import Contact from './views/Contact.vue';
+import Journal from './views/Journal.vue';
+import JournalItem from './views/JournalItem.vue';
+import MonitoringItem from './views/MonitoringItem.vue';
+import Monitorings from './views/Monitorings.vue';
+import References from './views/References.vue';
+import Services from './views/Services.vue';
 
 Vue.use(Router);
 
@@ -16,6 +23,41 @@ export default new Router({
       path: '/about',
       name: 'about',
       component: About,
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: Contact,
+    },
+    {
+      path: '/journal',
+      name: 'journal',
+      component: Journal,
+    },
+    {
+      path: '/journal/:id',
+      name: 'journalitem',
+      component: JournalItem,
+    },
+    {
+      path: '/monitorings',
+      name: 'monitorings',
+      component: Monitorings,
+    },
+    {
+      path: '/monitorings/:id',
+      name: 'monitoringsitem',
+      component: MonitoringItem,
+    },
+    {
+      path: '/references',
+      name: 'references',
+      component: References,
+    },
+    {
+      path: '/services',
+      name: 'services',
+      component: Services,
     },
   ],
 });
