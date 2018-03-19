1e84eec644bc31e623c20ab4dddb2f8f257c4b5c
Erdogan Bulut <ebulut@protel.com.tr>
2018-03-19 09:35:01 +0300

    Create and import reference component

---
+++ src/components/Reference.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="reference-component">
+    Reference Component
+  </section>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Reference extends Vue {
+  //  @Prop() private msg!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
--- src/views/References.vue
+++ src/views/References.vue
@@ -2,16 +2,19 @@
   <main>
     Referanslarımız
     <Banner />
+    <Reference />
   </main>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import Banner from '@/components/Banner.vue';
+import Reference from '@/components/Reference.vue';
 
 @Component({
   components: {
     Banner,
+    Reference,
   },
 })
 export default class References extends Vue {}
