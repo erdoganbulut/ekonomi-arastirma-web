117f1ef47aa75e73773d250b9dac80f31b950316
Erdogan Bulut <ebulut@protel.com.tr>
2018-03-19 09:42:23 +0300

    Create and import Contact Form and Content components

---
+++ src/components/ContactContent.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="contact-content-component">
+    ContactContent Component
+  </section>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class ContactContent extends Vue {
+  //  @Prop() private msg!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -2,16 +2,22 @@
   <main>
     İletişim
     <Banner />
+    <ContactContent />
+    <ContactForm />
   </main>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import Banner from '@/components/Banner.vue';
+import ContactContent from '@/components/ContactContent.vue';
+import ContactForm from '@/components/ContactForm.vue';
 
 @Component({
   components: {
     Banner,
+    ContactContent,
+    ContactForm,
   },
 })
 export default class Contact extends Vue {}
