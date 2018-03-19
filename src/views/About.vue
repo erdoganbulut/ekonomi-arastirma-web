fef3551b0cedc3fbebc892e840b523957093c7fe
Erdogan Bulut <ebulut@protel.com.tr>
2018-03-19 09:33:10 +0300

    Create and import employee component

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,16 +2,19 @@
   <main>
     Hakkımızda
     <Banner />
+    <Employee />
   </main>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import Banner from '@/components/Banner.vue';
+import Employee from '@/components/Employee.vue'
 
 @Component({
   components: {
     Banner,
+    Employee,
   },
 })
 export default class About extends Vue {}
