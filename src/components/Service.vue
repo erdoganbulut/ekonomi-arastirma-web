25827eab8f30adff289af183544d35233c54ceae
Erdogan Bulut <ebulut@protel.com.tr>
2018-03-19 09:33:28 +0300

    Create and import service component

---
+++ src/components/Service.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="service-component">
+    Service Component
+  </section>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Service extends Vue {
+  //  @Prop() private msg!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
--- src/views/Services.vue
+++ src/views/Services.vue
@@ -2,16 +2,19 @@
   <main>
     Hizmetlerimiz
     <Banner />
+    <Service />
   </main>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import Banner from '@/components/Banner.vue';
+import Service from '@/components/Service.vue';
 
 @Component({
   components: {
     Banner,
+    Service,
   },
 })
 export default class Services extends Vue {}
