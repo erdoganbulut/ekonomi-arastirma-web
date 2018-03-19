8e7af2651bca626594bf05ba045763fed419e1f0
Erdogan Bulut <ebulut@protel.com.tr>
2018-03-19 09:58:04 +0300

    Create sidebar components and import

--- src/views/JournalItem.vue
+++ src/views/JournalItem.vue
@@ -2,16 +2,28 @@
   <main>
     Gündem Item
     <Banner />
+    <Slogan />
+    <Content />
+    <BoxJournal />
+    <BoxTweet />
   </main>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import Banner from '@/components/Banner.vue';
+import Slogan from '@/components/Slogan.vue';
+import Content from '@/components/Content.vue';
+import BoxJournal from '@/components/BoxJournal.vue';
+import BoxTweet from '@/components/BoxTweet.vue';
 
 @Component({
   components: {
     Banner,
+    Slogan,
+    Content,
+    BoxJournal,
+    BoxTweet,
   },
 })
 export default class JournalItem extends Vue {}
--- src/views/MonitoringItem.vue
+++ src/views/MonitoringItem.vue
@@ -2,16 +2,28 @@
   <main>
     Türkiye Monitörü Item
     <Banner />
+    <Slogan />
+    <Content />
+    <BoxMonitoring />
+    <BoxTweet />
   </main>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import Banner from '@/components/Banner.vue';
+import Slogan from '@/components/Slogan.vue';
+import Content from '@/components/Content.vue';
+import BoxMonitoring from '@/components/BoxMonitoring.vue';
+import BoxTweet from '@/components/BoxTweet.vue';
 
 @Component({
   components: {
     Banner,
+    Slogan,
+    Content,
+    BoxMonitoring,
+    BoxTweet,
   },
 })
 export default class MonitoringItem extends Vue {}
