3232c09af14050f256e2618047ca68bfb13079ad
Erdogan Bulut <ebulut@protel.com.tr>
2018-03-21 18:21:57 +0300

    Import vue-resource

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,17 @@ import BootstrapVue from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 
+// Vue Resource
+import VueResource from 'vue-resource';
+
 import App from './App.vue';
 import router from './router';
 import store from './store/index';
 import Icons from './components/Icons.vue'
 
 Vue.use(BootstrapVue);
+
+Vue.use(VueResource);
 
 Vue.component('icon', Icons);
 
