7689d1b63dc721a0086985634aff30b6f4b522e9
Erdogan Bulut <ebulut@protel.com.tr>
2018-03-21 18:23:33 +0300

    Using home store module and change using mapaction

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,22 +6,28 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import { mapActions } from 'vuex';
 import Slider from '@/components/Slider.vue';
 
 @Component({
-  methods: {
-    ...mapActions({
-      setFooterVisibility: 'footer/setFooterVisibility',
-    }),
-  },
   components: {
     Slider,
   },
 })
 export default class Home extends Vue {
+  
+
+  setFooterVisibility(value: boolean) {
+    this.$store.dispatch('footer/setFooterVisibility', value);
+  }
+
+  getSliders() {
+    this.$store.dispatch('home/getSliders');
+  }
+
   mounted() {
     this.setFooterVisibility(false);
-  };
+    this.getSliders();
+  }
+
 };
 </script>
